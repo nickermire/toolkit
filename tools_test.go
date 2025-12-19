064741e67f750aa94602924b21a87ae19a207edf
Nick Stevermer <nickolas.stevermer@callmux.com>
2025-12-18 22:43:44 -0600

    added tests

--- tools_test.go
+++ tools_test.go
@@ -27,6 +27,8 @@ var uploadTests = []struct {
 	errorExpected bool
 }{
 	{name: "allowed no rename", allowedTypes: []string{"image/jpeg", "image/png"}, renameFile: false, errorExpected: false},
+	{name: "allowed rename", allowedTypes: []string{"image/jpeg", "image/png"}, renameFile: true, errorExpected: false},
+	{name: "not allowed", allowedTypes: []string{"image/jpeg"}, renameFile: false, errorExpected: true},
 }
 
 func TestTools_UploadFiles(t *testing.T) {
@@ -91,4 +93,47 @@ func TestTools_UploadFiles(t *testing.T) {
 		wg.Wait()
 
 	}
+}
+
+func TestTools_UploadOneFile(t *testing.T) {
+	
+	// set up a pipe to avoid buffering
+	pr, pw := io.Pipe()
+	writer := multipart.NewWriter(pw)
+	go func() {
+		defer writer.Close()
+		// create the form data field "file"
+		part, err := writer.CreateFormFile("file", "./testdata/test.png")
+		if err != nil {
+			t.Error(err)
+		}
+		f, err := os.Open("./testdata/test.png")
+		if err != nil {
+			t.Error(err)
+		}
+		defer f.Close()
+		img, _, err := image.Decode(f)
+		if err != nil {
+			t.Error("error decoding image", err)
+		}
+		err = png.Encode(part, img)
+		if err != nil {
+			t.Error(err)
+		}
+	}()
+	// read from the pipe which receives data
+	request := httptest.NewRequest("POST", "/", pr)
+	request.Header.Add("Content-Type", writer.FormDataContentType())
+	var testTools Tools
+	uploadedFiles, err := testTools.UploadOneFile(request, "./testdata/uploads/", true)
+	if err != nil {
+		t.Error(err)
+	}
+	
+	if _, err := os.Stat(fmt.Sprintf("./testdata/uploads/%s", uploadedFiles.NewFileName)); os.IsNotExist(err) {
+		t.Errorf("expected file to exist: %s", err.Error())
+	}
+	// clean up
+	_ = os.Remove(fmt.Sprintf("./testdata/uploads/%s", uploadedFiles.NewFileName))
+
 }
